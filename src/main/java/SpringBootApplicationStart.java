ed7530da160d0707d46218e1685227db4defd367
灿羽 <954877317@qq.com>
2018-01-18 14:45:41 +0800

    tijiao

---
+++ src/main/java/SpringBootApplicationStart.java
@@ -0,0 +1,33 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: zhaoyc
+ * @Description:
+ * @Date: Created in 9:59 2018/1/18
+ * @Modified:
+ */
+@EnableAutoConfiguration
+@Controller
+public class SpringBootApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationStart.class,args);
+    }
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "hello world!";
+    }
+    @RequestMapping("/welcome")
+    String welcome(Model model){
+        model.addAttribute("toUserName","小明");
+        model.addAttribute("message","哈哈");
+        model.addAttribute("fromUserName","小红");
+        return "t1/welcome";
+    }
+}
